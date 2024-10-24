0fd119048e24fced6b0cd58be59119e3186aeab8
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2024-10-25 00:48:30 +0800

    added UI for Task Screen

--- backend/src/main/java/com/project/taskify/repositories/TaskRepository.java
+++ backend/src/main/java/com/project/taskify/repositories/TaskRepository.java
@@ -1,9 +1,11 @@
 package com.project.taskify.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.project.taskify.models.TaskEntity;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Integer>{
+
+    Optional<TaskEntity> findByTitle(String title);
     
 }
--- frontend/src/components/FolderView/FolderView.jsx
+++ frontend/src/components/FolderView/FolderView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import './FolderView.css';
 
@@ -12,13 +12,87 @@ const Home = () => (
 );
 
 // Add code here for whos assigned to tasks tab
-const Tasks = () => (
-  /* Dont change className */
-  <div className="content tasks-content">
-    <h2>Add your content here</h2>
-    <p>Hello World</p>
-  </div>
-);
+//Reminder: Creation_date implementation that takes current date
+const Tasks = () => {
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+    priority: '',
+    creation_date: '',
+    due_date: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTask({ ...task, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log('Task data:', task);
+    setTask({
+      title: '',
+      description: '',
+      priority: '',
+      creation_date: '',
+      due_date: ''
+    });
+  };
+
+  return (
+    /* Dont change className */
+    <div className="content tasks-content">
+      <h3>Create New Task</h3>
+      <form onSubmit={handleSubmit}>
+        <input 
+        className='taskTitle'
+          type="text" 
+          name="title" 
+          placeholder="Title" 
+          value={task.title} 
+          onChange={handleChange} 
+          required 
+        />
+        <br></br>
+        <input 
+          className='taskDesc'
+          type="text" 
+          name="description" 
+          placeholder="Short description here..." 
+          value={task.description} 
+          onChange={handleChange} 
+           
+        />
+        <br></br>
+        <select 
+         className='taskPriority'
+          name="priority" 
+          value={task.priority} 
+          onChange={handleChange} 
+          required 
+        >
+          <option value="" disabled>Choose Priority</option>
+          <option className='high' value="high">High</option>
+          <option className='medium' value="medium">Medium</option>
+          <option className='low' value="low">Low</option>
+        </select>
+        <label for="due_date" className='dueDateLabel'>Due Date: </label>
+        <input 
+          className='taskDue'
+          type="date" 
+          name="due_date" 
+          value={task.due_date} 
+          onChange={handleChange} 
+          required 
+        />
+        <br></br>
+        <button className='addTask' type="submit">Add Task</button>
+      </form>
+    </div>
+  );
+};
+
 
 // Add code here for whos assigned to archive tab
 const Archive = () => (
