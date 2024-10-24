2266099bd7e6b842c7c562bbf0ff3e5cb36847f0
Felraine Kyle A. Cataos <felrainekyle.cataos@cit.edu>
2024-10-24 17:08:45 +0800

    Finished Main Content Area UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
+import FolderView from './components/FolderView/FolderView'; // New import for folder view
 
 const App = () => {
-
   const current_theme = localStorage.getItem('current_theme');
   const [theme, setTheme] = useState(current_theme ? current_theme : 'light');
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem('current_theme', theme);
-  },[theme])
+  }, [theme]);
 
   return (
     <div className={`container ${theme}`}>
-      <Home theme={theme} setTheme={setTheme}/>
+      <Home theme={theme} setTheme={setTheme} />
+      <FolderView /> {/* New component for folder-like UI */}
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
